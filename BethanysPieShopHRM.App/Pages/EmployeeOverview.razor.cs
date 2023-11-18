cd44bdedf77cff8c5f8e76ab8b93165002dcf596
Ian Chadburn <ian.chadburn@hotmail.co.uk>
2023-11-18 19:39:10 +0000

    Section 4 finished

--- BethanysPieShopHRM.App/Pages/EmployeeOverview.razor.cs
+++ BethanysPieShopHRM.App/Pages/EmployeeOverview.razor.cs
@@ -10,6 +10,8 @@ namespace BethanysPieShopHRM.App.Pages
 
         private Employee? _selectedEmployee;
 
+        private string Title = "Employee Overview";
+
         protected override void OnInitialized()
         {
             Employees = MockDataService.Employees;  
