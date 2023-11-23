c2238864e2b647f7f11fc7647a67c6ab45a3121c
Ian Chadburn <ian.chadburn@hotmail.co.uk>
2023-11-23 19:43:27 +0000

    Section 6 Creating a form finished.

---
+++ BethanysPieShopHRM.App/Pages/EmployeeEdit.razor.cs
@@ -0,0 +1,45 @@
+﻿using BethanysPieShopHRM.App.Services;
+using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+using System.Runtime.CompilerServices;
+
+namespace BethanysPieShopHRM.App.Pages
+{
+    public partial class EmployeeEdit
+    {
+        [Inject]
+        public IEmployeeDataService? EmployeeDataService { get; set;}
+
+        [Inject]
+        public ICountryDataService? CountryDataService { get; set;}
+
+        [Inject]
+        public IJobCategoryDataService? JobCategoryDataService { get; set;}
+
+
+        [Parameter]
+        public string? EmployeeID { get; set; }
+        public Employee Employee { get; set; } = new Employee();
+        public List<Country> Countries { get; set; } = new List<Country>();
+        public List<JobCategory> JobCategories { get; set; } = new List<JobCategory>();
+
+        protected async override Task OnInitializedAsync()
+        {
+            Countries = (await CountryDataService.GetAllCountries()).ToList();
+            JobCategories = (await JobCategoryDataService.GetAllJobCategories()).ToList();
+
+            int.TryParse(EmployeeID, out var employeeId);
+
+            if (employeeId == 0) //New employee is being created
+            {
+                //Add some defaults
+                Employee = new Employee { CountryId = 1, JobCategoryId = 1, BirthDate = DateTime.Now, JoinedDate = DateTime.Now};
+            }
+            else
+            {
+                Employee = await EmployeeDataService.GetEmployeeDetails(int.Parse(EmployeeID));
+            }
+        }
+
+    }
+}
